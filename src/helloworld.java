6cff8e9d45db8eff6174538872bda9fe617e8ecb
84327158672 <diephn2005@gmail.com>
2024-04-15 08:43:18 +0700

    hi

---
+++ src/helloworld.java
@@ -0,0 +1,6 @@
+public class helloworld {
+    public static void main ( String[] args ) {
+        System.out.println("Hello, Wold");
+        System.out.println("Hello java , I'm diep.");
+    }
+}
